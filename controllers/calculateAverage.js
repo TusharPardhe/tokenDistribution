e7782a99855b5d6cbabfc74462b9842a65f6ff9e
Tushar Pardhe <tushar.pardhe@airtel.com>
2022-04-21 21:47:36 +0530

    average calculation equation change

--- controllers/calculateAverage.js
+++ controllers/calculateAverage.js
@@ -8,44 +8,64 @@ const checkKycStatus = require("./checkKycStatus");
 
 require('dotenv').config({ path: path.resolve(__dirname, "../.env") });
 
+const previousAirdrops = [1223.8, 1327];
+
 const calcAvg = () => {
-    console.log("Calculating average...");
+    console.log("Calculating...\n\n");
 
     const { accountData: data } = JSON.parse(fs.readFileSync(accountJSONPath));
-    const totalLength = Object.keys(data).length;
+    const filteredAccounts = {};
+
     const minBalance = parseInt(process.env.MIN_HOLDER_BALANCE);
     const maxBalance = parseInt(process.env.MAX_HOLDER_BALANCE);
     const qualified_accounts = {};
     const excluded_accounts = JSON.parse(fs.readFileSync(excludedAccountsJSONPath));
 
-    let average = parseFloat(process.env.INCLUDE_IN_AVERAGE);
-    console.log(average);
-    Object.keys(data).map((acc) => {
+    const previousAirdropsSum = previousAirdrops.reduce((previousValue, currentValue) => previousValue + currentValue, 0.0);
+    const averageMultiplier = (100 - (5 * previousAirdrops.length)) / 100;
+    const firstPartOfEquation = averageMultiplier * previousAirdropsSum;
+    let average = 0.0;
+
+    // filter accounts based on max and min balances and remove excluded accounts
+    Object.keys(data).forEach((acc) => {
         const balance = data[acc].balance;
         if ((balance >= minBalance && balance <= maxBalance) && !excluded_accounts.includes(acc)) {
-            average += balance / totalLength;
-            // console.log(balance / totalLength);
+            filteredAccounts[acc] = balance;
         }
     });
 
-    Object.keys(data).map(acc => {
-        if (data[acc].balance > average && !excluded_accounts.includes(acc)) {
+    // calculate average of filtered accounts
+    const filteredAccKeys = Object.keys(filteredAccounts);
+    filteredAccKeys.forEach(acc => {
+        average += filteredAccounts[acc] / filteredAccKeys.length;
+    });
+
+    const fullAmount = firstPartOfEquation + average;
+
+    // get accounts that pass threshold
+    filteredAccKeys.forEach(acc => {
+        if (filteredAccounts[acc] > fullAmount) {
             qualified_accounts[acc]= false;
         }
     });
 
     const payload = {
-        average: average,
+        average: fullAmount,
         accountData: qualified_accounts,
     };
 
-    console.log("Average: ", average.toLocaleString("US"));
+    console.log("Previous airdrops sum (A):", previousAirdropsSum.toLocaleString("US"));
+    console.log("Multiplier (M):", averageMultiplier.toLocaleString("US"));
+    console.log("TL Average (T): ", average.toLocaleString("US"));
+    console.log("\nEquation: (A * M) + T", "\x1b[32m");
+    console.log("\nThreshold: ", fullAmount.toLocaleString("US"));
     console.log("Qualified Accounts: ", Object.keys(qualified_accounts).length.toLocaleString("US"));
+    console.log("Airdrop Amount: ", (4000000 / Object.keys(qualified_accounts).length).toLocaleString("US"), "\x1b[0m")
     
     fs.writeFileSync(qualifiedaccountJSONPath, JSON.stringify(payload, null, "\t"));
 
     if (process.env.CHECK_KYC == "true") {
-        checkKycStatus(average);
+        checkKycStatus(fullAmount);
     }
 };
 
